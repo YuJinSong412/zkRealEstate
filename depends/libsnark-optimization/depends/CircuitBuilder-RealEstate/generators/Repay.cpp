35a1987f52b03950ff2602484f5500d9ae00d76e
11lization <74034408+11lization@users.noreply.github.com>
2023-10-16 18:13:33 +0900

    Revert "first pull request"

--- depends/libsnark-optimization/depends/CircuitBuilder-RealEstate/generators/Repay.cpp
+++ depends/libsnark-optimization/depends/CircuitBuilder-RealEstate/generators/Repay.cpp
@@ -54,8 +54,6 @@ namespace CircuitBuilder
         void Repay::buildCircuit()
         {
 
-            //@@@Hi
-
             /* statements */
             CT_bond_balance = createInputWire("CT_bond_balance");
             cnt = createInputWire("cnt");
